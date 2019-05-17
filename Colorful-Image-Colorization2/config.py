166c753e300a70c41d1a160480b60d8ccc2a1989
Hara Kumar <hara.p.kumar@gmail.com>
2019-05-17 10:25:12 +0000

    View files in Colorful-Image-Coloriztion

---
+++ Colorful-Image-Colorization2/config.py
@@ -0,0 +1,14 @@
+img_rows, img_cols = 64, 64
+channel = 3
+batch_size = 1
+epochs = 50
+patience = 50
+num_train_samples = 10
+num_valid_samples = 10
+num_classes = 313
+kernel = 3
+weight_decay = 1e-3
+epsilon = 1e-8
+nb_neighbors = 5
+# temperature parameter T
+T = 0.38
