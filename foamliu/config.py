909192f232edc5a5fba3456f2b661e74ab2e630d
harakumar <40324083+harakumar@users.noreply.github.com>
2019-05-18 14:38:22 +0200

    Update config.py to consume full dataset

--- foamliu/config.py
+++ foamliu/config.py
@@ -1,10 +1,10 @@
 img_rows, img_cols = 32, 32
 channel = 3
-batch_size = 10
-epochs = 5
-patience = 50
-num_train_samples = 40
-num_valid_samples = 10
+batch_size = 100
+epochs = 50
+patience = 10
+num_train_samples = 40000
+num_valid_samples = 10000
 num_classes = 241
 kernel = 3
 weight_decay = 1e-3
